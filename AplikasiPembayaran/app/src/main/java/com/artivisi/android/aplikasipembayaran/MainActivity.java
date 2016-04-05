2cd65e1a5d7c410113b6d60b5a87198368910e23
Endy Muhardin <endy.muhardin@gmail.com>
2016-04-05 11:49:18 +0700

    implementasi fragment

--- AplikasiPembayaran/app/src/main/java/com/artivisi/android/aplikasipembayaran/MainActivity.java
+++ AplikasiPembayaran/app/src/main/java/com/artivisi/android/aplikasipembayaran/MainActivity.java
@@ -2,13 +2,17 @@ package com.artivisi.android.aplikasipembayaran;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.artivisi.android.aplikasipembayaran.fragment.DashboardFragment;
 
 /**
  * Created by endymuhardin on 4/5/16.
@@ -18,12 +22,14 @@ public class MainActivity extends AppCompatActivity {
     private String[] menus = {"Menu 1", "Menu 2", "Menu 3"};
     private DrawerLayout drawerLayout;
     private ListView listView;
+    DashboardFragment dashboardFragment = new DashboardFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main_layout);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
@@ -38,11 +44,14 @@ public class MainActivity extends AppCompatActivity {
 
     private class DrawerItemClickListener implements AdapterView.OnItemClickListener {
 
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             switch(position) {
                 case 0:
-                    Toast.makeText(getApplicationContext(), "menu 1", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(getApplicationContext(), "menu 1", Toast.LENGTH_SHORT).show();
+                    fragmentTransaction.replace(R.id.content_frame, dashboardFragment);
                     break;
                 case 1:
                     Toast.makeText(getApplicationContext(), "menu 2", Toast.LENGTH_SHORT).show();
@@ -51,6 +60,9 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "menu 3", Toast.LENGTH_SHORT).show();
                     break;
             }
+
+            fragmentTransaction.commit();
+            drawerLayout.closeDrawers();
         }
     }
 }
---
+++ AplikasiPembayaran/app/src/main/java/com/artivisi/android/aplikasipembayaran/fragment/DashboardFragment.java
@@ -0,0 +1,26 @@
+package com.artivisi.android.aplikasipembayaran.fragment;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.artivisi.android.aplikasipembayaran.R;
+
+/**
+ * Created by endymuhardin on 4/5/16.
+ */
+public class DashboardFragment extends Fragment {
+
+    private View rootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.dashboard_layout, container, false);
+        return rootView;
+    }
+}
