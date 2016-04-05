3cd85c247d8ad6357049bbcec802454cfc5e3457
Endy Muhardin <endy.muhardin@gmail.com>
2016-04-05 10:46:38 +0700

    click listener di item nav drawer

--- AplikasiPembayaran/app/src/main/java/com/artivisi/android/aplikasipembayaran/MainActivity.java
+++ AplikasiPembayaran/app/src/main/java/com/artivisi/android/aplikasipembayaran/MainActivity.java
@@ -3,13 +3,17 @@ package com.artivisi.android.aplikasipembayaran;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by endymuhardin on 4/5/16.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private String[] menus = {"Menu 1", "Menu 2", "Menu 3"};
     private DrawerLayout drawerLayout;
@@ -20,6 +24,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main_layout);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         listView = (ListView) findViewById(R.id.left_drawer);
 
@@ -27,6 +33,24 @@ public class MainActivity extends Activity {
         listView.setAdapter(new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, menus));
         // Set the list's click listener
-//        listView.setOnItemClickListener(new DrawerItemClickListener());
+        listView.setOnItemClickListener(new DrawerItemClickListener());
+    }
+
+    private class DrawerItemClickListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            switch(position) {
+                case 0:
+                    Toast.makeText(getApplicationContext(), "menu 1", Toast.LENGTH_SHORT).show();
+                    break;
+                case 1:
+                    Toast.makeText(getApplicationContext(), "menu 2", Toast.LENGTH_SHORT).show();
+                    break;
+                case 2:
+                    Toast.makeText(getApplicationContext(), "menu 3", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
     }
 }
