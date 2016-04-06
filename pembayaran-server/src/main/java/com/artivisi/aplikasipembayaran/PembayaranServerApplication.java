f2fb0bf05dbfb8438bddc3dae17582d35598f168
Endy Muhardin <endy.muhardin@gmail.com>
2016-04-06 08:35:44 +0700

    template project web

---
+++ pembayaran-server/src/main/java/com/artivisi/aplikasipembayaran/PembayaranServerApplication.java
@@ -0,0 +1,12 @@
+package com.artivisi.aplikasipembayaran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PembayaranServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PembayaranServerApplication.class, args);
+	}
+}
---
+++ pembayaran-server/src/test/java/com/artivisi/aplikasipembayaran/PembayaranServerApplicationTests.java
@@ -0,0 +1,18 @@
+package com.artivisi.aplikasipembayaran;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PembayaranServerApplication.class)
+@WebAppConfiguration
+public class PembayaranServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
