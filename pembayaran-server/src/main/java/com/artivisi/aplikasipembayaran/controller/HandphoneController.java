8e7c8fa7b86bce413ca0a0d6e7ff6a5fbe59e33b
Endy Muhardin <endy.muhardin@gmail.com>
2016-04-07 16:56:08 +0700

    refactor URL

--- pembayaran-server/src/main/java/com/artivisi/aplikasipembayaran/controller/HandphoneController.java
+++ pembayaran-server/src/main/java/com/artivisi/aplikasipembayaran/controller/HandphoneController.java
@@ -14,12 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
+@RequestMapping(value = "/api/user")
 public class HandphoneController {
 
     @Autowired private PembayaranService pembayaranService;
     @Autowired private UserDao userDao;
 
-    @RequestMapping(value = "/api/user/{username}/handphone", method = RequestMethod.PUT)
+    @RequestMapping(value = "/{username}/handphone", method = RequestMethod.PUT)
     public void updateHandphoneUser(@PathVariable String username,
                                     @RequestBody Map<String, String> data) {
 
